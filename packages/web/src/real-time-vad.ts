73f485197e15ff53dc649fa977c775be77297044
Jett Scythe <jettscythe@hotmail.com>
2024-10-10 14:45:19 -0300

    https://github.com/ricky0123/vad/pull/100

--- packages/web/src/real-time-vad.ts
+++ packages/web/src/real-time-vad.ts
@@ -13,7 +13,7 @@ import { OrtOptions, Silero, SpeechProbabilities } from "./models"
 
 interface RealTimeVADCallbacks {
   /** Callback to run after each frame. The size (number of samples) of a frame is given by `frameSamples`. */
-  onFrameProcessed: (probabilities: SpeechProbabilities) => any
+  onFrameProcessed: (probabilities: SpeechProbabilities, frame: Float32Array) => any
 
   /** Callback to run if speech start was detected but `onSpeechEnd` will not be run because the
    * audio segment is smaller than `minSpeechFrames`.
@@ -71,7 +71,7 @@ export type RealTimeVADOptions =
 
 export const defaultRealTimeVADOptions: RealTimeVADOptions = {
   ...defaultFrameProcessorOptions,
-  onFrameProcessed: (probabilities) => {},
+  onFrameProcessed: (probabilities: SpeechProbabilities, frame: Float32Array) => {},
   onVADMisfire: () => {
     log.debug("VAD misfire")
   },
@@ -270,10 +270,11 @@ export class AudioNodeVAD {
       probs: SpeechProbabilities
       msg: Message
       audio: Float32Array
+      frame: Float32Array
     }>
   ) => {
     if (ev.probs !== undefined) {
-      this.options.onFrameProcessed(ev.probs)
+      this.options.onFrameProcessed(ev.probs, ev.frame as Float32Array)
     }
     switch (ev.msg) {
       case Message.SpeechStart:
